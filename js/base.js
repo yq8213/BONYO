d488b6e7d7e385d56e64cbd8dcf776ac27962813
yq8213 <yq8213@gmail.com>
2021-03-28 12:29:37 +0800

    更新到今年

--- js/base.js
+++ js/base.js
@@ -36,14 +36,19 @@ $(function(){
 	$('#contact_submit').click(function(){		
 		_name = $("#name").val();
 		_email = $("#email").val();
+		var reEmail = /\S+@\S+.\S+/;
 		_message = $("#message").val();
-		if(_name!==""&&_email!==""&&_message!==""){
+		if(_name!==""&&reEmail.test(_email)&&_message!==""){
 	 		$('.pop_box').attr('onclick','send_box()')
 	 		$.getScript("https://docs.google.com/forms/d/e/1FAIpQLSfQHPeAH_nOR9TFRA3h5c3ErNthP1rTqua-5MgRfetWw3-Cug/formResponse?entry.358507525="+ _name +"&entry.1166596340="+ _email +"&entry.1436156744=" + _message);
 			$('.pop_message p').text('您的留言已成功送出♡')
 		}else{
 			$('.pop_box').attr('onclick','error_box()')
-			$('.pop_message p').text('請確實填寫完畢再送出喔♡')
+			if(_email!=="" && !reEmail.test(_email)){
+				$('.pop_message p').text('請填寫正確的email喔♡')
+			}else{
+				$('.pop_message p').text('請確實填寫完畢再送出喔♡')
+			}
 		}
 		$('.pop_box').toggleClass('pop_show');
 		$('.wrapper').toggleClass('body_hid');
